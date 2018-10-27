9af0c4350d92521cb09861c22177499da556a0a4
Emma Remy <remy.eem@gmail.com>
2018-10-26 22:24:04 -0400

    working by some definition

--- pumpkin.py
+++ pumpkin.py
@@ -3,12 +3,12 @@ import cv2
 from sys import argv
 
 def morph_ops(mask):
-    little_kernel = np.ones((3,3), np.uint8)
-    kernel = np.ones((5,5), np.uint8)
-    big_kernel = np.ones((9,9), np.uint8)
+    little_kernel = np.ones((2,2), np.uint8)
+    kernel = np.ones((3,3), np.uint8)
+    big_kernel = np.ones((5,5), np.uint8)
     mask = cv2.morphologyEx(mask, cv2.MORPH_DILATE, little_kernel, iterations=1)
     mask = cv2.morphologyEx(mask, cv2.MORPH_OPEN, big_kernel, iterations=5)
-    smoothed = cv2.blur(mask, (19, 19))
+    smoothed = cv2.blur(mask, (3, 3))
     ret, smoothed = cv2.threshold(smoothed, 0, 255, cv2.THRESH_BINARY+cv2.THRESH_OTSU)
     return smoothed
 
@@ -30,27 +30,25 @@ def main():
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
     # Segment
-    ret, mask = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY)
+    ret, mask = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY+cv2.THRESH_OTSU)
     mask = morph_ops(mask)
     mask_inv = cv2.bitwise_not(mask)
 
-    # TODO: add morph_ops
-
     # Load pumpkin
     light = cv2.imread('light.jpg')
-    light_size = cv2.resize(light, pum.shape[0:2])
+    light = cv2.resize(light, (roi.shape[1], roi.shape[0]))
 
     print(img.shape)
     print(mask_inv.shape)
     print(roi.shape)
 
-    pum_bg = cv2.bitwise_and(roi, roi, mask = np.uint8(mask_inv))
-    light_bg = cv2.bitwise_and(img, img, mask = np.uint8(mask))
+    pum_bg = cv2.bitwise_and(roi, roi, mask = np.uint8(mask))
+    light_bg = cv2.bitwise_and(light, light, mask = np.uint8(mask_inv))
     both = cv2.add(pum_bg, light_bg)
     pum[0:rows, 0:cols] = both
 
     # Save final image
     img_path_split = img_path.split('.')
-    cv2.imwrite(img_path_split[0] + '_pumpkin.png', both)
+    cv2.imwrite(img_path_split[0] + '_pumpkin.png', pum)
 
 main()
